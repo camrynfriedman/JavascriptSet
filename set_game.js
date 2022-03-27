cc3517d1b8c847c50089b17c45fb0423f00b0a2f
Bencp2 <Pearlstein.14@osu.edu>
2022-03-27 14:45:03 -0400

    cards now print

--- set_game.js
+++ set_game.js
@@ -7,13 +7,20 @@ class Card{
         this.imageName = imgName;
     }
 
-    get symb(){
-        return this.symbol;
-    }
-    get image(){
-        return this.imageName;
-    }
+    // get symb(){
+    //     return this.symbol;
+    // }
+    // get image(){
+    //     return this.imageName;
+    // }
 }
+
+// class Image{
+//     constructor(src){
+//         this.source = src
+//     }
+// }
+
 
 const cards = [];
 const symbols = ["oval", "squiggle", "diamond"];
@@ -26,9 +33,8 @@ for (let i of symbols){
         for (let k of numSyms){
             for (let l of shades){
                 //so that the right image is pulled given the attributes i, j, k, and l
-                let img = new Image();
-                img.src = 'images/' + k.charAt(0) + i.charAt(0) + l.charAt(0) + j.charAt(0) + '.png'; 
-                let card = new Card(i, j, k, l, img); 
+                //let img = new Image();
+                let card = new Card(i, j, k, l, 'images/' + k.charAt(0) + i.charAt(0) + l.charAt(0) + j.charAt(0) + '.png'); 
                 cards.push(card);
             }
         }
@@ -41,20 +47,24 @@ for (let i = 0; i < 12; i++){
     let draw = Math.floor(Math.random() * cards.length);
     tableCards.push(cards.splice(draw,1)); //splice removes element from cards and pushes it to the tableCards array
 }
-
-function displayCards(){
+console.log(tableCards[0]);
+//function displayCards(){
+  //  console.log(tableCards);
     let table = document.querySelector("table");
     let i = 0;
+    let j = 0;
     for (let x = 0; x<4; x++){//rows
         let row = table.insertRow();
         for(let y = 0; y<3; y++){//columns
             let cell = row.insertCell();
-            let card = tableCards[i];
+            let card = tableCards[i][j];
             //.innerHTML is used to get/set the HTML content of an element node
-            cell.innerHTML = '<img src = "' + card.image + '">'; //NEED TO GET CARD'S SOURCE!!
+            cell.innerHTML = '<img src = "' + card.imageName + '">'; //NEED TO GET CARD'S SOURCE!!
             i++;
+
         }
+        j++
     }
-}
-console.log(tableCards[0]) //correct path to image shows in console under currentSource
-displayCards();
+//}
+//console.log(tableCards) //correct path to image shows in console under currentSource
+//displayCards();
