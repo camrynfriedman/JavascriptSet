0aa9337da3519906a08d690720a46568c78fa11e
aisharun7 <aisharun7@gmail.com>
2022-03-27 15:02:44 -0400

    final changes to displayCards function

--- set_game.js
+++ set_game.js
@@ -49,24 +49,20 @@ for (let i = 0; i < 12; i++){
     tableCards.push(array[0]); //splice removes element from cards and pushes it to the tableCards array
     
 }
-console.log(tableCards[0]);
-//function displayCards(){
-  //  console.log(tableCards);
+
+function displayCards(){
     let table = document.querySelector("table");
     let i = 0;
 
-    for (let x = 0; x<4; x++){//rows
+    for (let x = 0; x<3; x++){//rows
         let row = table.insertRow();
-        for(let y = 0; y<3; y++){//columns
+        for(let y = 0; y<4; y++){//columns
             let cell = row.insertCell();
             let card = tableCards[i];
             //.innerHTML is used to get/set the HTML content of an element node
             cell.innerHTML = '<img src = "' + card.imageName + '">'; //NEED TO GET CARD'S SOURCE!!
             i++;
-
         }
-
     }
-//}
-//console.log(tableCards) //correct path to image shows in console under currentSource
-//displayCards();
+}
+displayCards();
